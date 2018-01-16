a639654771bb27ebbfc71248b72fbcd691f6c65b
luoh1358 <luoh1358>
2018-01-16 15:19:50 +0800

    添加依赖

--- plogin/src/main/java/com/lehow/plogin/di/NetModule.java
+++ plogin/src/main/java/com/lehow/plogin/di/NetModule.java
@@ -5,12 +5,13 @@ import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module public class NetModule {
 
-  public static final String HOST = BuildConfig.
+  public static final String HOST = BuildConfig.API_HOST_URL;
   @Provides OkHttpClient
-
   provideOkHttpClient() {
     return new OkHttpClient.Builder().build();
   }
