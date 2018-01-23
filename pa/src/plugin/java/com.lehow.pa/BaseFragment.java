45d94b312414d3b262dab573b6da408b21d0fddc
luoh1358 <luoh1358>
2018-01-23 11:44:14 +0800

    添加遗漏的引用

--- pa/src/plugin/java/com.lehow.pa/BaseFragment.java
+++ pa/src/plugin/java/com.lehow.pa/BaseFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import butterknife.ButterKnife;
 import com.qihoo360.replugin.RePlugin;
 
 public abstract class BaseFragment extends Fragment {
