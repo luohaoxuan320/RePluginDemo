e9a072af2236e09fc4b908aa7b7edbc9f292c6c1
luoh1358 <luoh1358>
2018-04-13 12:16:32 +0800

    删除多余的测试代码

--- plogin/main/src/main/java/com/lehow/plogin/LoginPresenter.java
+++ plogin/main/src/main/java/com/lehow/plogin/LoginPresenter.java
@@ -2,7 +2,6 @@ package com.lehow.plogin;
 
 import android.text.TextUtils;
 import android.util.Log;
-import com.lehow.loading.ILoadingView;
 import com.lehow.loading.LoadingSubscriber;
 import com.lehow.plogin.base.RxLifeCycleHelper;
 import com.lehow.plogin.biz.LoginBiz;
@@ -12,13 +11,8 @@ import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.FlowableSubscriber;
-import io.reactivex.FlowableTransformer;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
-import java.util.concurrent.Callable;
-import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscription;
 
 public class LoginPresenter implements LoginContract.Presenter {
@@ -74,8 +68,7 @@ public class LoginPresenter implements LoginContract.Presenter {
       }
     });*/
 
-    dialogWithCompose();
-    //dialogWithUsing();
+    dialogWithUsing();
     if (true) return;
 
     //TODO 基本的本地校验逻辑
@@ -118,64 +111,6 @@ public class LoginPresenter implements LoginContract.Presenter {
     this.loginView = null;
   }
 
-  /**
-   * 这个必须放在subscribe之前，紧贴着subscribe，否则doOnComplete可能不会被调用
-   */
-  public <T> FlowableTransformer waitLoadingTransformerDoOn(final ILoadingView iLoadingView) {
-
-    return new FlowableTransformer<T, T>() {
-      @Override public Publisher<T> apply(Flowable<T> upstream) {
-        return upstream.doOnSubscribe(new Consumer<Subscription>() {
-          @Override public void accept(Subscription subscription) throws Exception {
-            Log.i(TAG, "doOnSubscribe accept: ");
-            iLoadingView.show();
-          }
-        }).doOnNext(new Consumer() {
-          @Override public void accept(Object o) throws Exception {
-            Log.i(TAG, "doOnNext accept: ");
-          }
-        }).doOnError(new Consumer<Throwable>() {
-          @Override public void accept(Throwable throwable) throws Exception {
-            Log.i(TAG, "doOnError accept: ");
-            iLoadingView.dismiss();
-          }
-        }).doOnComplete(new Action() {
-          @Override public void run() throws Exception {
-            Log.i(TAG, "doOnComplete run: ");
-            iLoadingView.dismiss();
-          }
-        });
-      }
-    };
-  }
-
-  public <T> FlowableTransformer waitLoadingTransformerUsing(final ILoadingView iLoadingView) {
-
-    return new FlowableTransformer<T, T>() {
-      @Override public Publisher<T> apply(final Flowable<T> upstream) {
-        return Flowable.using(new Callable<ILoadingView>() {
-          @Override public ILoadingView call() throws Exception {
-            Log.i(TAG, "using call: 11");
-            //初始化
-            iLoadingView.show();
-            return iLoadingView;
-          }
-        }, new Function<ILoadingView, Publisher<? extends T>>() {
-          @Override public Publisher<? extends T> apply(ILoadingView iLoadingView)
-              throws Exception {
-            Log.i(TAG, "using apply: 22");
-            return upstream;
-          }
-        }, new Consumer<ILoadingView>() {
-          @Override public void accept(ILoadingView iLoadingView) throws Exception {
-            Log.i(TAG, "using accept:333 ");
-            //释放资源
-            iLoadingView.dismiss();
-          }
-        });
-      }
-    };
-  }
 
   private void dialogWithUsing() {
 
@@ -197,7 +132,7 @@ public class LoginPresenter implements LoginContract.Presenter {
         }
       }
     }, BackpressureStrategy.BUFFER)
-        .compose(waitLoadingTransformerUsing(loginView))
+        .compose(rxLifeCycleHelper.waitLoadingTransformerUsing(loginView))
         .compose(rxLifeCycleHelper.io_main())
         .compose(rxLifeCycleHelper.bindTolifecycle(RxLifeCycleHelper.ActivityEvent.DESTROY))
         .subscribe(new FlowableSubscriber<Long>() {
@@ -220,45 +155,4 @@ public class LoginPresenter implements LoginContract.Presenter {
         });
   }
 
-  private void dialogWithCompose() {
-    Flowable.create(new FlowableOnSubscribe<Long>() {
-      @Override public void subscribe(FlowableEmitter<Long> emitter) throws Exception {
-        for (int i = 1; i > 0; i++) {
-          Log.i(TAG, "subscribe: i=" + i + " Thread=" + Thread.currentThread());
-          emitter.onNext((long) i);
-          try {
-            Thread.sleep(2 * 1000);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-            return;
-          }
-          if (i == 10) {
-            emitter.onError(new Throwable("TTT"));
-            return;
-          }
-        }
-      }
-    }, BackpressureStrategy.BUFFER)
-        .compose(rxLifeCycleHelper.io_main())
-        .compose(rxLifeCycleHelper.bindTolifecycle(RxLifeCycleHelper.ActivityEvent.DESTROY))
-        .compose(waitLoadingTransformerDoOn(loginView))
-        .subscribe(new FlowableSubscriber<Long>() {
-          @Override public void onSubscribe(Subscription s) {
-            Log.i(TAG, "onSubscribe: ");
-            s.request(Integer.MAX_VALUE);
-          }
-
-          @Override public void onNext(Long aLong) {
-            Log.i(TAG, "onNext: " + aLong);
-          }
-
-          @Override public void onError(Throwable t) {
-            Log.i(TAG, "onError: " + t);
-          }
-
-          @Override public void onComplete() {
-            Log.i(TAG, "onComplete: ");
-          }
-        });
-  }
 }
--- plogin/main/src/main/java/com/lehow/plogin/base/RxLifeCycleHelper.java
+++ plogin/main/src/main/java/com/lehow/plogin/base/RxLifeCycleHelper.java
@@ -1,13 +1,17 @@
 package com.lehow.plogin.base;
 
+import com.lehow.loading.ILoadingView;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableTransformer;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.BehaviorSubject;
+import java.util.concurrent.Callable;
 import org.reactivestreams.Publisher;
 
 public class RxLifeCycleHelper {
@@ -32,6 +36,31 @@ public class RxLifeCycleHelper {
       @Override public Publisher<T> apply(Flowable<T> upstream) {
         return upstream.takeUntil(
             takeUntilEvent(lifecycleSubject, lifeEvent).toFlowable(BackpressureStrategy.LATEST));
+      }
+    };
+  }
+
+  public <T> FlowableTransformer waitLoadingTransformerUsing(final ILoadingView iLoadingView) {
+
+    return new FlowableTransformer<T, T>() {
+      @Override public Publisher<T> apply(final Flowable<T> upstream) {
+        return Flowable.using(new Callable<ILoadingView>() {
+          @Override public ILoadingView call() throws Exception {
+            //初始化
+            iLoadingView.show();
+            return iLoadingView;
+          }
+        }, new Function<ILoadingView, Publisher<? extends T>>() {
+          @Override public Publisher<? extends T> apply(ILoadingView iLoadingView)
+              throws Exception {
+            return upstream;
+          }
+        }, new Consumer<ILoadingView>() {
+          @Override public void accept(ILoadingView iLoadingView) throws Exception {
+            //释放资源
+            iLoadingView.dismiss();
+          }
+        });
       }
     };
   }
