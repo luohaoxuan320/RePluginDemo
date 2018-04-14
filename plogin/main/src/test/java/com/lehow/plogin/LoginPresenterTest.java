9c7222b37bc4574d63fac0f8e5a6edfc2f02ae74
luoh1358 <luoh1358>
2018-04-14 15:07:47 +0800

    去掉多余代码

--- plogin/main/src/test/java/com/lehow/plogin/LoginPresenterTest.java
+++ plogin/main/src/test/java/com/lehow/plogin/LoginPresenterTest.java
@@ -6,14 +6,10 @@ import io.reactivex.Flowable;
 import io.reactivex.Scheduler;
 import io.reactivex.android.plugins.RxAndroidPlugins;
 import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
-import io.reactivex.internal.schedulers.ExecutorScheduler;
 import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.schedulers.Schedulers;
 import java.util.concurrent.Callable;
-import java.util.concurrent.Executor;
-import java.util.concurrent.TimeUnit;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -31,33 +27,7 @@ public class LoginPresenterTest {
   private LoginPresenter loginPresenter;
 
   @Before public void setupLoginPresenter() {
-
-    System.out.println("setupLoginPresenter");
-
-    final Scheduler immediate = new Scheduler() {
-
-      @Override
-      public Disposable scheduleDirect(@NonNull Runnable run, long delay, @NonNull TimeUnit unit) {
-        return super.scheduleDirect(run, 0, unit);
-      }
-
-      @Override public Worker createWorker() {
-        return new ExecutorScheduler.ExecutorWorker(new Executor() {
-          @Override public void execute(@NonNull Runnable command) {
-            command.run();
-          }
-        });
-      }
-    };
-
-    // reset()不是必要，实践中发现不写reset()，偶尔会出错，所以写上保险^_^
     RxAndroidPlugins.reset();
-   /* RxAndroidPlugins.initMainThreadScheduler(new Callable<Scheduler>() {
-      @Override public Scheduler call() throws Exception {
-        return immediate;
-        //return Schedulers.trampoline();
-      }
-    });*/
 
     RxAndroidPlugins.setInitMainThreadSchedulerHandler(
         new Function<Callable<Scheduler>, Scheduler>() {
@@ -70,7 +40,6 @@ public class LoginPresenterTest {
     RxJavaPlugins.reset();
     RxJavaPlugins.setIoSchedulerHandler(new Function<Scheduler, Scheduler>() {
       @Override public Scheduler apply(Scheduler scheduler) throws Exception {
-        //return immediate;
         return Schedulers.trampoline();
       }
     });
